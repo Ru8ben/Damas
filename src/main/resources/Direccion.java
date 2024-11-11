e229d58884f764bb07af49275cbbd4ec1b5bf840
Ru8ben <hernanruben62@gmail.com>
2024-11-11 23:35:53 +0100

    commit

---
+++ src/main/resources/Direccion.java
@@ -0,0 +1,11 @@
+public enum Direccion {
+    NORESTE("Noreste"),
+    SURESTE("Sureste"),
+    NOROESTE("Noroeste"),
+    SUROESTE("Suroeste"),
+    ;
+
+    private String toString() {
+        return "Direccion{}";
+    }
+}
