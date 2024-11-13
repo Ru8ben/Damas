632b5caca3e9cf93b5208d81f5796e29ebdbd16d
Ru8ben <hernanruben62@gmail.com>
2024-11-13 18:00:04 +0100

    commit

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -1,2 +1,64 @@
 public class MainApp {
+    private static Dama dama;
+
+    public static void main(String[] args) {
+        int opcion;
+        do {
+            Consola.mostrarMenu();
+            opcion = Consola.elegirOpcionMenu();
+            ejecutarOpcion(opcion);
+        } while (opcion != 4);
+    }
+
+    private static void ejecutarOpcion(int opcion) {
+        switch (opcion) {
+            case 1 -> crearDamaDefecto();
+            case 2 -> crearDamaColor();
+            case 3 -> mover();
+            case 4 -> Consola.despedirse();
+            default -> System.out.println("Opción no válida");
+        }
+    }
+
+    private static void crearDamaDefecto() {
+        dama = new Dama();
+        System.out.println("Dama creada por defecto.");
+        mostrarDama();
+    }
+
+    private static void crearDamaColor() {
+        Color color = Consola.elegirColor();
+        dama = new Dama(color);
+        System.out.println("Dama creada con color elegido.");
+        mostrarDama();
+    }
+
+    private static void mover() {
+        if (dama == null) {
+            System.out.println("No hay dama creada. Crea una dama primero.");
+            return;
+        }
+
+        Direccion direccion = Consola.elegirDireccion();
+        int pasos = 1;
+        if (dama.isEsDamaEspecial()) {
+            pasos = Consola.elegirPasos();
+        }
+
+        try {
+            dama.mover(direccion, pasos);
+            System.out.println("Dama movida con éxito.");
+            mostrarDama();
+        } catch (OperationNotSupportedException e) {
+            System.out.println("Error al mover la dama: " + e.getMessage());
+        }
+    }
+
+    private static void mostrarDama() {
+        if (dama != null) {
+            System.out.println(dama);
+        } else {
+            System.out.println("No hay dama creada.");
+        }
+    }
 }
